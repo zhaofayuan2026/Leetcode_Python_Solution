7392fff2b22f027f57031afbaa45abb1e3ab1cca
zhaofayuan <2798237059@qq.com>
2026-08-15 15:44:13 +0800

    Add files via upload
    修改：利用集合减少时间复杂度，时间复杂度O（n）

--- 0041_first_missing_positive1.0.py
+++ 0041_first_missing_positive1.0.py
@@ -1,8 +1,10 @@
 class Solution:
     def firstMissingPositive(self,nums):
         i=1
+        set_nums=set(nums)
+
         while True:
-            if i in nums:
+            if i in set_nums:
                 i=i+1
             else:
                 return i
